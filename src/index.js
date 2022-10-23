e160f33acdd1cb75280409f726cb04ac205f95c9
Maria <claranegrao11@gmail.com>
2022-10-23 14:28:39 -0300

    Função teste para agrupar todas notas em um só array

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 const fs = require("fs/promises");
 const { convertTxtFileInObject } = require("./utils/convertTxtFileInObject");
 
-async function readingFirstNote() {
+async function convertAllNotesInArray() {
+	let allNotes = [];
 	try {
-		const note = await fs.readFile("./Notas/N1.txt");
-		console.log(convertTxtFileInObject(note.toString()));
+		const quantityOfNotes = await fs.readdir("./Notas");
+		for (let i = 1; i < quantityOfNotes.length + 1; i++) {
+			const note = await fs.readFile(`./Notas/N${i}.txt`);
+			const noteData = convertTxtFileInObject(note.toString());
+			allNotes.push(noteData);
+		}
+		console.log(allNotes);
 	} catch (error) {
 		console.log(error);
 	}
 }
 
-readingFirstNote();
+convertAllNotesInArray();
